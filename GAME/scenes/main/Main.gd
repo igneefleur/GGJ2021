e63d057b5ffd88f771a26024fd5c905a2d683d6c
Flo-authentique <florian.duzes@gmail.com>
2021-01-30 19:53:17 +0100

    les menus c'est FINIS git add -A!

--- GAME/scenes/main/Main.gd
+++ GAME/scenes/main/Main.gd
@@ -22,27 +22,28 @@ func _on_Menu_principal_lanceOption():
 func _on_OptionMenu_retour():
 	if security:
 		$OptionMenu.hide()
+		$MenuPause.show()
 	else:
 		$OptionMenu.hide()
-		$"Menu principal".hide()
+		$"Menu principal".show()
 
 func _on_MenuPause_OptionPressed():
 	$OptionMenu.show()
-	security = false
+
 
 
 func _on_MenuPause_pausePressed():
 	if $MenuPause.pause and security:
 		print("pause", $MenuPause.pause)
-		$Map.get_tree().paused = true
+		#$Map.paused = true
 		$MenuPause.show()
 	else:
 		$MenuPause.hide()
-		$Map.get_tree().paused = false
+		#$Map.paused = false
 
 
 func _on_MenuPause_resumePressed():
-	$Map.get_tree().paused = false
-	#$MenuPause.hide
+	$MenuPause.pause = true
+	$MenuPause.hide()
 	
 	
